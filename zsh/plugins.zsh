884b0e5b9afd9f7e766585c60a7ea4874e66241c
iff <iff@yvesineichen.com>
2018-01-23 07:53:44 +0100

    zsh::plugins: add more assumingly useful plugins

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,3 +1,10 @@
 antigen bundle psprint/history-search-multi-word
-antigen apply
 
+antigen use oh-my-zsh
+antigen bundle command-not-found
+antigen bundle cp
+antigen bundle git
+antigen bundle pip
+antigen bundle docker
+
+antigen apply

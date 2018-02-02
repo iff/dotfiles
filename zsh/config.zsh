e2d6321aff4b475f2abf22985a3c91e483d34314
iff <iff@yvesineichen.com>
2018-02-02 12:45:45 +0100

    zsh: enable vim mode, history changes

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -8,12 +8,11 @@ export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
 fpath=($ZSH/zsh/functions $fpath)
-
 autoload -U $ZSH/zsh/functions/*(:t)
 
 HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=10000000
+SAVEHIST=10000000
 
 setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
@@ -26,25 +25,34 @@ setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 setopt CORRECT
 setopt COMPLETE_IN_WORD
-#setopt IGNORE_EOF #disables ctrl + d to logout
 
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt HIST_IGNORE_DUPS
 setopt HIST_REDUCE_BLANKS
+setopt HIST_EXPIRE_DUPS_FIRST
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
 setopt complete_aliases
 
-zle -N newtab
-
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
-#bindkey '\e[3~' delete-char
 bindkey '^R' history-incremental-search-backward
+
+# try vim mode for zle
+bindkey -v
+export KEYTIMEMOUT=1 # quicker reaction to mode change (might interfere with other things) (1=0.1seconds)
+# Use vim cli mode
+bindkey '^P' up-history
+bindkey '^N' down-history
+# backspace and ^h working even after
+# returning from command mode
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+# ctrl-w removed word backwards
+bindkey '^w' backward-kill-word
+# ctrl-r starts searching history backward
+#bindkey '^r' history-incremental-search-backward
+#bindkey '^r' znt-history-widget
+bindkey '^r' history-search-multi-word # todo not sure why I have to do it here, the plugin does it already, but it doesnt work
+# edit command line in editor
+#bindkey '^x^e' edit-command-line

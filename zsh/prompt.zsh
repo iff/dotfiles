72960eb407b899501e3369670064bf0c882d3d33
iff <iff@yvesineichen.com>
2018-08-06 17:04:29 +0200

    zsh::prompt: more updates, highlight bg

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -12,26 +12,27 @@ need_push () {
   then
     echo " "
   else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
+    echo " with %F{magenta}%Bunpushed%b "
   fi
 }
 
 directory_name(){
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+  echo "%F{cyan}%1/%\/%f"
 }
 
-export PROMPT=$'\n%{$fg_bold[yellow]%}%n%{$reset_color%} in $(directory_name) ${vcs_info_msg_0_}$(need_push)\n› '
+export PROMPT=$'
+%(?,,%F{red}%?%f)
+%K{10}%F{cyan}%n%f in $(directory_name) ${vcs_info_msg_0_}$(need_push)%F{yellow}%T%f %(1j,%F{red}%j&%f,) %E%k
+› '
 
 precmd() {
   title "zsh" "%m" "%55<...<%~"
 
   if [[ -z $(git ls-files --other --exclude-standard 2> /dev/null) ]] {
-    zstyle ':vcs_info:git*' formats 'on %F{green}%b%f%u%c'
+    zstyle ':vcs_info:git*' formats 'on %F{green}%b%f%F{red}%u%c%f'
   } else {
-    zstyle ':vcs_info:git*' formats 'on %F{red}%b%f%u%c'
+    zstyle ':vcs_info:git*' formats 'on %F{red}%b%f%F{red}%u%c%f'
   }
 
   vcs_info
-
-  set_prompt
 }

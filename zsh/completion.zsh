7e32de788410b57e767567ae89935ee05b9b128e
iff <iff@yvesineichen.com>
2018-08-15 07:24:21 +0200

    zsh: use Daniels nice ..

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,16 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# go back to a parent folder (default first parent)
+.. () {
+        1=${1:-$(basename $(dirname $(pwd)))}
+        while [[ $(basename $(pwd)) != $1 ]]; do
+                cd ..
+        done
+}
+_complete_.. () {
+        reply=$(pwd)
+        reply=(${(s|/|)reply})
+}
+compctl -K _complete_.. ..

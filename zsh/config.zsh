c85fde861cac10005c23bd774455c4f1241ee5f6
Yves Ineichen <iff@yvesineichen.com>
2019-12-13 17:10:01 +0100

    zsh:: history no dups

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -29,8 +29,11 @@ setopt EXTENDED_HISTORY # add timestamps to history
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_DUPS
+setopt HIST_FIND_NO_DUPS
 setopt HIST_REDUCE_BLANKS
 setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_SAVE_NO_DUPS
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]

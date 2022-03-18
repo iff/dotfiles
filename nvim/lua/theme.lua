832e0398f3e7b5465a55558e7e0a52d1e0982173
Yves Ineichen <iff@yvesineichen.com>
2022-03-18 08:59:18 +0100

    nvim: configure lualine

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -6,6 +6,35 @@ require('nightfox').load('nordfox')
 --require('nightfox').load('dawnfox')
 
 -- lualine
-require('lualine').setup {options = { theme = 'nightfox' }}
+-- require('lualine').setup {options = { theme = 'gruvbox' }}
+-- require('lualine').setup {options = { theme = 'nightfox' }}
 require('nvim-web-devicons').setup {}
 
+require('lualine').setup {
+  options = {
+    theme = 'gruvbox',
+    icons_enabled = false,
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {},
+    always_divide_middle = true,
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {'quickfix'}
+}

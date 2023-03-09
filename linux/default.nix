cd285cc92a178348cf966a84721c64b8d3ceed96
Yves Ineichen <iff@yvesineichen.com>
2023-03-09 12:56:23 +0100

    redshift: config with nix

--- linux/default.nix
+++ linux/default.nix
@@ -28,6 +28,22 @@ in
     pkgs.redshift
   ];
 
-  home.file.".config/redshift.conf".source = ../redshift/redshift.conf;
-
+  services = {
+    redshift = {
+      enable = true;
+      temperature = {
+        day = 5700;
+        night = 3200;
+      };
+      provider = "manual";
+      latitude = 47.4;
+      longitude = 8.5;
+      settings = {
+        redshift.adjustment-method = "randr";
+        redshift.transition = 1;
+        redshift.brightness-day = 1.0;
+        redshift.brightness-night = 0.7;
+      };
+    };
+  };
 }

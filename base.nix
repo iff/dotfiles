ce7711245adce03142d24e569fa529a6e4c804ea
Yves Ineichen <iff@yvesineichen.com>
2023-03-11 16:28:19 +0100

    nixify git config

--- base.nix
+++ base.nix
@@ -80,7 +80,6 @@ in
     git-ssh-dispatch
     pkgs.direnv
     pkgs.exa
-    pkgs.git
     pkgs.jq
     pkgs.nix-direnv
     pkgs.tmux
@@ -94,7 +93,87 @@ in
     enableZshIntegration = true;
   };
 
+  programs.git = {
+    enable = true;
+    userName = "Yves Ineichen";
+    signing.key = "165AEDEB";
+
+    extraConfig = {
+      core = {
+        sshCommand = "git-ssh-dispatch";
+        pager = "less -FRSX";
+        editor = "nvim";
+        # excludesfile = /home/iff/.gitignore
+      };
+      color = {
+        ui = "true";
+        diff = "auto";
+      };
+      rerere = {
+        enabled = 1;
+      };
+
+      merge = {
+        tool = "nvim";
+      };
+    };
+
+    aliases = {
+      s = "status";
+      b = "branch";
+      ba = "branch -a -v -v";
+      bs = "!git-branch-status";
+      bsi = "!git-branch-status -i";
+
+      sw = "!git checkout $(git branch -a --format '%(refname:short)' | sed 's~origin/~~' | sort | uniq | fzf)";
+
+      ci = "commit";
+      co = "checkout";
+
+      d = "diff -C";
+      ds = "diff -C --stat";
+      dsp = "diff -C --stat -p";
+      dw = "diff -C --color-words";
+
+      l = "log -C --decorate";
+      ls = "log -C --stat --decorate";
+      lsp = "log -C --stat -p --decorate";
+
+      lc = "log ORIG_HEAD.. --stat --no-merges";
+
+      lg = "log --graph '--pretty=tformat:%Cblue%h%Creset %Cgreen%ar%Creset %Cblue%d%Creset %s'";
+      lga = "log --graph '--pretty=tformat:%Cblue%h%Creset %Cgreen%ar%Creset %Cblue%d%Creset %s' --all";
+      l19 = "log --graph '--pretty=tformat:%Cblue%h%Creset %Cgreen%ar%Creset %Cblue%d%Creset %s' --all -19";
+      lsd = "log --graph '--pretty=tformat:%Cblue%h%Creset %Cgreen%ar%Creset %Cblue%d%Creset %s' --all --simplify-by-decoration";
+      lgr = "log --graph '--pretty=tformat:%Cblue%h%Creset %Cgreen%ar%Creset %Cblue%d%Creset %s' --first-parent";
+
+      ru = "remote update";
+
+      # "show-branch -g=N" can't be aliased for N easily, so we stop here:
+      sb = "show-branch --sha1-name";
+
+      ls-del = "ls-files -d";
+      ls-mod = "ls-files -m";
+      ls-new = "ls-files --exclude-standard -o ";
+      ls-ign = "ls-files --exclude-standard -o -i";
+
+      whois = "!sh -c 'git log -i -1 --pretty=\"format:%an <%ae>\n\" --author=\"$1\"' -";
+      whatis = "show -s --pretty='tformat:%h (%s, %ad)' --date=short";
+
+      # locate = "!sh -c 'git ls-files "\\ * $1 "' -";
+
+      # edit-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; vim `f`";
+      # add-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; git add `f`";
+
+      edit-unmerged = "!f() { git diff --name-status --diff-filter=U | cut -f2 ; }; v `f`";
+      add-unmerged = "!f() { git diff --name-status --diff-filter=U | cut -f2 ; }; git add `f`";
+
+      # fixup
+      fu = "commit --amend --reset-author -C HEAD";
+      fuu = "!git add -u && git fu";
+    };
+  };
+
   # TODO: switch to hm config
-  home.file.".gitconfig".source = ./git/gitconfig;
   home.file.".tmux.conf".source = ./tmux/tmux.conf;
 }

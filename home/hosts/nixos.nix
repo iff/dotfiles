c96a26d66ccc725b56b3938f1f243889651c3d4c
Yves Ineichen <iff@yvesineichen.com>
2023-06-02 15:02:20 +0200

    add nixos config to hm

---
+++ home/hosts/nixos.nix
@@ -0,0 +1,13 @@
+{ ... }:
+
+{
+  home.stateVersion = "23.05";
+
+  dots = {
+    profiles = {
+      linux.enable = true;
+    };
+    alacritty.font_size = 13.0;
+    #  TODO ../modules/programs/hyprland.nix
+  };
+}

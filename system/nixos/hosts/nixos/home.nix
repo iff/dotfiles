1dfedd3bbfe9ebbf75eed1b663dbf98e5e12b5a6
Yves Ineichen <iff@yvesineichen.com>
2023-09-02 12:27:37 +0200

    removing nixos home and moving hyprland conf

--- system/nixos/hosts/nixos/home.nix
+++ system/nixos/hosts/nixos/home.nix
@@ -3,10 +3,15 @@
 {
   home.packages = with pkgs; [
     pkgs.geeqie
-    # TODO
+    # pkgs.vscode
   ];
 
+  services.syncthing.enable = true;
+
   dots = {
+    # profiles = {
+    #   hyprland.enable = true;
+    # };
     alacritty.enable = true;
     alacritty.font_size = 12.0;
   };

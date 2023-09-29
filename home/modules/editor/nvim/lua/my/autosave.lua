3672717db50ff9cedb8dba87af8d8ad43a49ea61
Yves Ineichen <iff@yvesineichen.com>
2023-09-29 11:26:51 +0200

    nvim: update autosave

--- home/modules/editor/nvim/lua/my/autosave.lua
+++ home/modules/editor/nvim/lua/my/autosave.lua
@@ -1,13 +1,29 @@
 local mod = {}
 
 local function callback(context)
-    if vim.bo.buftype ~= '' then
+    if vim.b.autosave == false then
+        return
+    end
+
+    if
+        -- no file
+        context.file == ''
         -- special buffers (help, terminal, popups, ...) dont need saving
+        or vim.bo.buftype ~= ''
+        -- buffers that disappear anyway, probably they are not meant to be saved
+        or (vim.bo.bufhidden == 'wipe' or vim.bo.bufhidden == 'delete')
+        -- fugitive special buffers (eg, diff view where write means stage changes, do it explicitely)
+        or string.find(context.file, '^fugitive://') ~= nil
+        -- efs is slow
+        or string.find(context.file, '^/efs/') ~= nil
+        -- neogit commits the automatic comments in the message when using autosave (?)
+        or vim.bo.filetype == 'NeogitCommitMessage'
+    then
+        vim.b.autosave = false
         return
     end
-    if string.find(context.file, '^/efs/') ~= nil then
-        return
-    end
+    vim.b.autosave = true
+
     -- :update only saves if the file has been modified, no-op otherwise
     -- :silent prevents "xyz bytes write" from popping up everytime, but also hides error messages
     vim.cmd('silent update')

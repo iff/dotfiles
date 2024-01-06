5c341f106a37dba96622f412e8a6a4d69a04a250
Yves Ineichen <iff@yvesineichen.com>
2024-01-06 07:10:09 +0100

    nvim: man pages and help pages keybindings and telescope

--- home/modules/editor/nvim.nix
+++ home/modules/editor/nvim.nix
@@ -162,4 +162,24 @@ in
 
   home.file.".config/nvim/init.lua".source = ./nvim/init.lua;
   home.file.".config/nvim/lua".source = ./nvim/lua;
+  home.file.".config/nvim/ftplugin/help.vim".text =
+    ''
+      " see also /usr/local/share/nvim/runtime/ftplugin/help.vim
+      nmap <silent><buffer> go gO<c-w>c<cmd>lua require("telescope.builtin").loclist({fname_width=0})<enter>
+    '';
+  home.file.".config/nvim/ftplugin/man.vim".text =
+    ''
+      " use this to find an entry interactively
+      " TODO could also do something with telescope if we parse it using some
+      " heuristics
+      nmap <buffer> f /\C^ *
+      nmap <buffer> - /\C^ *-
+
+      " see also /usr/local/share/nvim/runtime/ftplugin/man.vim
+      nnoremap <silent> <buffer> k <cmd>set scroll=0<enter><c-u><c-u>
+      nnoremap <silent> <buffer> h <cmd>set scroll=0<enter><c-d><c-d>
+      " TODO this mapping is not very well aligned yet
+      " not bad, a bit hacky to "hide" the filename column
+      nnoremap <silent> <buffer> go <cmd>lua require("man").show_toc()<enter><c-w>c<cmd>lua require("telescope.builtin").loclist({fname_width=0})<enter>
+    '';
 }

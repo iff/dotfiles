ee4d00e585bf47e90b4db7a9075578c8d90a4399
Yves Ineichen <iff@yvesineichen.com>
2024-10-19 14:24:56 +0200

    nixos: new machine

--- system/nixos/hosts/nixos/hardware.nix
+++ system/nixos/hosts/nixos/hardware.nix
@@ -3,11 +3,10 @@
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
   boot = {
-    kernelModules = [ "kvm-intel" ];
+    kernelModules = [ "kvm-amd" ];
     extraModulePackages = [ ];
     initrd = {
-      # availableKernelModules = [ "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdmmc" ];
-      availableKernelModules = [ "xhci_pci" "ahci" "usbhid" ];
+      availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" ];
       kernelModules = [ "dm-snapshot" ];
     };
 
@@ -21,23 +20,20 @@
 
   };
 
+  swapDevices =
+    [{ device = "/dev/disk/by-uuid/346d26c9-5545-4606-bbc8-17a7a620369f"; }];
+
+
   fileSystems."/" =
     {
-      device = "/dev/disk/by-uuid/0e96cc4b-43f2-4671-adce-d1db204da693";
-      fsType = "ext4";
+      device = "/dev/disk/by-label/nixos";
+      fsType = "btrfs";
     };
   fileSystems."/boot/efi" =
     {
-      device = "/dev/disk/by-uuid/9F4C-752E";
+      device = "/dev/disk/by-label/boot";
       fsType = "vfat";
     };
-  fileSystems."/pics" =
-    {
-      device = "/dev/disk/by-uuid/b898a75d-5616-42a6-9e64-7ead819e21e5";
-      fsType = "ext4";
-    };
 
-  swapDevices = [ ];
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  # powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 }

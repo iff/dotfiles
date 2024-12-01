589556ff55ba531a6bccb5fc88287cf55604806e
Yves Ineichen <iff@yvesineichen.com>
2024-12-01 07:22:36 +0100

    nvim: treesitter

--- home/modules/editor/nvim/lua/my/treesitter.lua
+++ home/modules/editor/nvim/lua/my/treesitter.lua
@@ -1,11 +1,28 @@
 local mod = {}
 
 function mod.setup()
+    local query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { 'BufWrite', 'CursorHold' },
+    }
+
     require('nvim-treesitter.configs').setup({
         highlight = {
             enable = true,
-            -- additional_vim_regex_highlighting = false,
         },
+        incremental_selection = {
+            enable = false,
+            -- TODO try
+            -- keymaps = {
+            --     init_selection = "gnn",
+            --     node_incremental = "grn",
+            --     scope_incremental = "grc",
+            --     node_decremental = "grm",
+            -- },
+        },
+        indent = { enable = false },
+        query_linter = query_linter,
     })
 end
 
